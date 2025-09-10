4c759b2d0268a59770608866ba2f51d8a1c8dc7d
Tom Keddie <thomaskeddie@gmail.com>
2025-09-10 08:19:37 +0000

    that didnt fix it.  can you try one of the other recommended fixes pleas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,6 @@ export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
-  interactiveWidget: 'resizes-content',
 }
 
 export default function RootLayout({
